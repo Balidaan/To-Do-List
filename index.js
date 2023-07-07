e97bce29bbf6b7ccdad27d0072241bc2b4541030
Mankirat <sainiversatile123@gmail.com>
2023-07-07 14:32:52 +0530

    adding enter key press

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function addMore() {
   var input1 = document.getElementById("input").value;
   var error1 = document.getElementById("error");
-
+  console.log(this);
   if (input1 == "" || input1 == " ") {
     error1.innerHTML = "Please enter a value";
     error1.style.color = "red";
@@ -23,6 +23,13 @@ function addMore() {
     a.className = "remove";
     li.appendChild(a);
 
+    a.addEventListener("click", function(e) {
+      let box = document.getElementById("box");
+      let li = e.target.parentNode;
+      console.log(e.target.parentNode);
+     li.remove();
+    });
+
     var x = document.createElement("input");
     x.type = "radio";
     li.appendChild(x);
@@ -38,9 +45,13 @@ function addMore() {
   document.getElementById("input").value = "";
 }
 
-//let btn = document.querySelector("ul");
-a.addEventListener("click", function (e) {
-  let box = document.getElementById("box");
-  let li = e.target.parentNode;
-  box.removeChild(li);
-});
+
+
+var btn = document.querySelector("#input");
+btn.addEventListener("keyup", function(e){
+  if(e.key=="Enter"){
+    addMore();  }
+
+})
+
+
